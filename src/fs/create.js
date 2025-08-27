f8487957e4c388e875fdb4e584e9ab1142c0f7af
Yevhen Kliuchyk <kliuchyk@gmail.com>
2025-08-27 16:33:10 +0200

    add create new file solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises as fs } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-  // Write your code here
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+
+  try {
+    await fs.writeFile(filePath, "I am fresh and young", { flag: "wx" });
+    console.log("File created successfully!");
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
